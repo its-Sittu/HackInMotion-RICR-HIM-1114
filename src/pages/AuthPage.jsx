0747a5cf97e96bc988ecb40137cf6ad200e04422
unknown <sitturaj730@gmail.com>
2026-08-14 15:33:04 +0530

    Redesign AuthPage brand to MEDISAFE and update auth.css with cinematic landing page theme

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -57,7 +57,7 @@ function Logo() {
           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
         </svg>
       </div>
-      <span className="auth-logo-text">PulseMed</span>
+      <span className="auth-logo-text">MEDISAFE</span>
     </div>
   )
 }
